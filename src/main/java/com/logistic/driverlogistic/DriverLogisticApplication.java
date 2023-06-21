e6ff3a6451d6b3462e3e239611776e2e2f37c331
AndreyZ <a.zahar3111981@gmail.com>
2023-06-21 19:46:43 +0300

    start project

---
+++ src/main/java/com/logistic/driverlogistic/DriverLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.driverlogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverLogisticApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DriverLogisticApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/logistic/driverlogistic/DriverLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistic.driverlogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverLogisticApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
